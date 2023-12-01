61b373295473265593c67be6c7a018090263b1bc
Pieter Schreurs <Pieter_j_Schreurs@hotmail.com>
2023-12-01 21:30:17 +0100

    Part 2, day 1

--- Year2023/Week1/ConsoleApp1/Program.cs
+++ Year2023/Week1/ConsoleApp1/Program.cs
@@ -1,11 +1,9 @@
 ﻿using System.IO;
 using System.Text;
+using System.Text.RegularExpressions;
 
-int totalNumber = 0;
-int number1 = 0;
-int number2 = 0;
-
-string[] letters = new string[] { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+int sum = 0;
+const string DIGIT_PATTERN = @"one|two|three|four|five|six|seven|eight|nine|[0-9]";
 
 using (FileStream fileStream = File.Open(Path.Combine(Directory.GetCurrentDirectory(), "Input.txt"), FileMode.Open, FileAccess.Read))
 {
@@ -13,97 +11,32 @@ using (FileStream fileStream = File.Open(Path.Combine(Directory.GetCurrentDirect
     {
         while (streamReader.Peek() >= 0)
         {
-
             string line = streamReader.ReadLine();
-            int indexStart = line.Length;
-            int indexLast = 0;
-            int numberfoundFirst = 0;
-            int numberFoundSecond = 0;
-            char[] ch = new char[line.Length];
-            for (int i = 0; i < letters.Length; i++)
-            {
-                if (line.Contains(letters[i]))
-                {
-                    int index = line.IndexOf(letters[i]);
-                    if (index < indexStart)
-                    {
-                        indexStart = index;
-                        numberfoundFirst = i + 1;
-                    }
-                    if (numberfoundFirst != 0 && index >= indexLast)
-                    {
-                        indexLast = index;
-                        numberFoundSecond = i + 1;
-                    }
-                }
-            }
-
-            for (int i = 0; i < line.Length; i++)
-            {
-                ch[i] = line[i];
-            }
-            List<int> numbersInLine = new List<int>();
-            int numberToAdd = 0;
-            int firstNumberDigit = 0;
-            int lastNumberDigit = 0;
-
-            foreach (char c in ch)
-            {
-                bool canConvert = int.TryParse(c.ToString(), out number1);
-                if (canConvert)
-                {
-                    numbersInLine.Add(number1);
-                    if (firstNumberDigit == 0)
-                    {
-                        firstNumberDigit = Array.IndexOf(ch, c);
-                    }
-                    else if (lastNumberDigit == 0)
-                    {
-                        lastNumberDigit = Array.IndexOf(ch, c);
-                    }
-                }
-            }
-            if (numbersInLine.Count > 0)
-            {
-                if (firstNumberDigit < indexStart)
-                {
-                    numberToAdd = numbersInLine.First() * 10;
-
-                }
-                else
-                {
-                    numberToAdd = numberfoundFirst * 10;
-
-                }
-
-                if (lastNumberDigit < indexLast)
-                {
-                    if (numbersInLine.Last() % 10 == 0) { numberToAdd += (numberToAdd / 10); }
-                    else { numberToAdd += numbersInLine.Last(); }
-
-                }
-                else
-                {
-                    if (numberFoundSecond % 10 == 0) { numberToAdd += (numberToAdd / 10); }
-                    else { numberToAdd += numberFoundSecond; }
-                }
-            }
-            else
-            {
-                Console.WriteLine("error");
-            }
-
-            Console.WriteLine(numberToAdd);
-            totalNumber += numberToAdd;
-
-
-
-            //Console.WriteLine("\tFirst char of text has index " + indexStart + " and has a value of " + numberfoundFirst + " : " + indexLast + " / " + numberFoundSecond);
-            //Console.WriteLine("Second char " + firstNumberDigit + " : " + numbersInLine.First() + ":" );
+            var first = Regex.Match(line, DIGIT_PATTERN);
+            var last = Regex.Match(line, DIGIT_PATTERN, RegexOptions.RightToLeft);
+            sum += 10 * GetValue(first.Value) + GetValue(last.Value);
         }
+
+        Console.WriteLine(sum);
     }
 }
-Console.WriteLine(totalNumber);
+
+static int GetValue(string s)
+{
+    return s switch
+    {
+        "one" => 1,
+        "two" => 2,
+        "three" => 3,
+        "four" => 4,
+        "five" => 5,
+        "six" => 6,
+        "seven" => 7,
+        "eight" => 8,
+        "nine" => 9,
+        _ => int.Parse(s)
+    };
+}
 
 
 /* Part 1
