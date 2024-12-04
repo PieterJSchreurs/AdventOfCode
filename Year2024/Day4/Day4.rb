515836aacfe4d1404fa9f0ad56df1d05fa09481d
Pieter <PieterS@Rometron.nl>
2024-12-04 18:09:25 +0100

    Added part 2

--- Year2024/Day4/Day4.rb
+++ Year2024/Day4/Day4.rb
@@ -1,4 +1,5 @@
 $wordToFind = 'XMAS'.chars
+$wordToFindPart2 = 'MAS'.chars
 $rowsMax = 0
 $columsMax = 0
 $twoDArray = []
@@ -17,31 +18,64 @@ def read_file
 end
 
 def findWord(row, column, letterIndex, horizontal, vertical)
-  return unless $twoDArray[row][column] === $wordToFind[letterIndex]
+  # return unless $twoDArray[row][column] === $wordToFind[letterIndex]
+  return false unless $twoDArray[row][column] === $wordToFind[letterIndex]
 
-  if letterIndex === $wordToFind.length - 1
-    $wordsFound += 1
-  else
-    # Check if last row
-    return if row + horizontal >= $rowsMax || row + horizontal < 0
-    return if column + vertical >= $columnsMax || column + vertical < 0
+  # if letterIndex === $wordToFind.length - 1
+  return true if letterIndex === $wordToFind.length - 1
 
-    findWord(row + horizontal, column + vertical, letterIndex + 1, horizontal, vertical)
+  # Check if last row or column
+  return false if row + horizontal >= $rowsMax || row + horizontal < 0
+  return false if column + vertical >= $columnsMax || column + vertical < 0
+
+  findWord(row + horizontal, column + vertical, letterIndex + 1, horizontal, vertical)
+end
+
+def findWordPart2(row, column, letterIndex, horizontal, vertical)
+  return false if letterIndex != 1
+
+  # Check if new pos is an M or S
+  isM = findLetterForPosition(row + horizontal, column + vertical, 0)
+  isS = findLetterForPosition(row + horizontal, column + vertical, 2)
+
+  if isM
+    otherIsS = findLetterForPosition(row - horizontal, column - vertical, 2)
+    return true if otherIsS
   end
+  if isS
+    otherIsM = findLetterForPosition(row - horizontal, column - vertical, 0)
+    return true if otherIsM
+  end
+  false
+end
+
+def findLetterForPosition(row, column, letterIndex)
+  return false if row >= $rowsMax || row < 0
+
+  return false if column >= $columnsMax || column < 0
+
+  $twoDArray[row][column] === $wordToFindPart2[letterIndex]
 end
 
 read_file
 
 $twoDArray.each_with_index do |sub_array, i|
   sub_array.each_with_index do |item, j|
-    findWord(i, j, 0, 1, 0)
-    findWord(i, j, 0, 1, 1)
-    findWord(i, j, 0, 1, -1)
-    findWord(i, j, 0, -1, 0)
-    findWord(i, j, 0, -1, 1)
-    findWord(i, j, 0, -1, -1)
-    findWord(i, j, 0, 0, 1)
-    findWord(i, j, 0, 0, -1)
+    # findWord(i, j, 0, 1, 0)
+    # findWord(i, j, 0, 1, 1)
+    # findWord(i, j, 0, 1, -1)
+    # findWord(i, j, 0, -1, 0)
+    # findWord(i, j, 0, -1, 1)
+    # findWord(i, j, 0, -1, -1)
+    # findWord(i, j, 0, 0, 1)
+    # findWord(i, j, 0, 0, -1)
+    next unless $twoDArray[i][j] === $wordToFindPart2[1]
+
+    trueCounts = 0
+    trueCounts += 1 if findWordPart2(i, j, 1, 1, 1)
+    trueCounts += 1 if findWordPart2(i, j, 1, 1, -1)
+
+    $wordsFound += 1 if trueCounts >= 2
   end
 end
 
